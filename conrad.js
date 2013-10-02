363dba12ae3bf57a19a0ea0c8c97319f3cdbdc76
conradj <conradsjackson@gmail.com>
2013-10-02 08:33:45 +0100

    Update conrad.js
    Betterised it

--- conrad.js
+++ conrad.js
@@ -1,3 +1,6 @@
-for (var conrad = 0; conrad == -1; conrad++) { 
+var never = -1;
+var endTheAwesomeness = never;
+
+for (var conrad = 0; conrad == endTheAwesomeness; conrad++) { 
   document.writeln("Conrad is frickin aweome! Yeah, I said aweome!");
 }
